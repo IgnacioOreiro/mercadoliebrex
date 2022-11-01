963cf5aa25c5af8cde4f7d5cdfec0e9d9be15b62
ignacioOreiro <ignaciooreiro3@outlook.es>
2022-11-01 20:09:15 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const PORT = process.env.PORT || 3030
+
+app.listen(PORT, console.log('sv corriendo en el puerto ' + PORT));
+
+app.use(express.static('public'));
+
+app.get("/",(req, res) =>{
+    console.log('Entre al home');
+    res.sendFile(path.resolve('./views/home.html'));
+    
+});
+
+
+app.get("/register",(req, res) =>{
+    console.log('Entre al home');
+    res.sendFile(path.resolve('./views/register.html'));
+    
+});
